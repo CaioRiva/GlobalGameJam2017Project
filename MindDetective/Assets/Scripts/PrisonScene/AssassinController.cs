1f8a4832af74802188e74f7e563d077eedbec572
Gaseta <matheus.gaseta97@gmail.com>
2018-01-27 07:47:50 -0200

    [Commit003]
    Adding prison scene basis

---
+++ MindDetective/Assets/Scripts/PrisonScene/AssassinController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssassinController : GenericSpriteController
+{
+
+	void Start () {
+        SetGameObjectForSpriteRenderer(this.gameObject);
+    }
+	
+	
+	void Update () {
+		
+	}
+}
